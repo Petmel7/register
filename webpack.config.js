ae43731aa1a1739ec855127733cedd20396413ae
Petmel7 <115225282+Petmel7@users.noreply.github.com>
2023-10-14 20:39:14 +0300

    add

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "name": "webpack",
+    "version": "5.89.0",
+    "author": "Tobias Koppers @sokra",
+    
+    "resolve": {
+        "fallback": {
+            "http": require.resolve('stream-http')
+        }
+    },
+};
