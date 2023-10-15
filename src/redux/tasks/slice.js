28f11a9067ee0cd1c1f8ef89e6b5ca6a5aad5c18
Petmel7 <115225282+Petmel7@users.noreply.github.com>
2023-10-15 18:45:48 +0300

    add

--- src/redux/tasks/slice.js
+++ src/redux/tasks/slice.js
@@ -18,86 +18,38 @@ const tasksSlice = createSlice({
         isLoading: false,
         error: null,
     },
-    extraReducers: {
-        [fetchTasks.pending]: handlePending,
-        [addTask.pending]: handlePending,
-        [deleteTask.pending]: handlePending,
-        [fetchTasks.rejected]: handleRejected,
-        [addTask.rejected]: handleRejected,
-        [deleteTask.rejected]: handleRejected,
-        [fetchTasks.fulfilled](state, action) {
-            state.isLoading = false;
-            state.error = null;
-            state.items = action.payload;
-        },
-        [addTask.fulfilled](state, action) {
-            state.isLoading = false;
-            state.error = null;
-            state.items.push(action.payload);
-        },
-        [deleteTask.fulfilled](state, action) {
-            state.isLoading = false;
-            state.error = null;
-            const index = state.items.findIndex(
-                task => task.id === action.payload.id
-            );
-            state.items.splice(index, 1);
-        },
-        [logOut.fulfilled](state) {
-            state.items = [];
-            state.error = null;
-            state.isLoading = false;
-        },
-    },
 
-
-
-    // extraReducers: (builder) => {
-    //     builder
-    //         .addCase(fetchTasks.pending, handlePending)
-    //         .addCase(addTask.pending, handlePending)
-    //         .addCase(deleteTask.pending, handlePending)
-    //         .addCase(fetchTasks.rejected, handleRejected)
-    //         .addCase(addTask.rejected, handleRejected)
-    //         .addCase(deleteTask.rejected, handleRejected)
-    //         .addCase(fetchTasks.fulfilled, (state, action) => {
-    //             state.isLoading = false;
-    //             state.error = null;
-    //             state.items = action.payload;
-    //         })
-    //         // .addCase(addTask.fulfilled, (state, action) => {
-    //         //     state.isLoading = false;
-    //         //     state.error = null;
-    //         //     state.items.push(action.payload);
-    //         // })
-
-    //         .addCase(addTask.fulfilled, (state, action) => {
-    //             state.isLoading = false;
-    //             state.error = null;
-    //             state.items.push(action.payload);
-    //         })
-    //         // .addCase(deleteTask.fulfilled, (state, action) => {
-    //         //     state.isLoading = false;
-    //         //     state.error = null;
-    //         //     const index = state.items.findIndex(
-    //         //         task => task.id === action.payload.id
-    //         //     );
-    //         //     state.items.splice(index, 1);
-        
-    //         .addCase(deleteTask.fulfilled, (state, action) => {
-    //             state.isLoading = false;
-    //             state.error = null;
-    //             state.items = state.items.filter(
-    //                 task => task.id !== action.payload.id
-    //             )
-    //         })
-    //         .addCase(logOut.fulfilled, (state) => {
-    //             state.items = [];
-    //             state.error = null;
-    //             state.isLoading = false;
-    //         });
-    // }
-
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchTasks.pending, handlePending)
+            .addCase(addTask.pending, handlePending)
+            .addCase(deleteTask.pending, handlePending)
+            .addCase(fetchTasks.rejected, handleRejected)
+            .addCase(addTask.rejected, handleRejected)
+            .addCase(deleteTask.rejected, handleRejected)
+            .addCase(fetchTasks.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.error = null;
+                state.items = action.payload;
+            })
+            .addCase(addTask.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.error = null;
+                state.items.push(action.payload);
+            })
+            .addCase(deleteTask.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.error = null;
+                state.items = state.items.filter(
+                    task => task.id !== action.payload.id
+                )
+            })
+            .addCase(logOut.fulfilled, (state) => {
+                state.items = [];
+                state.error = null;
+                state.isLoading = false;
+            })
+    }
 });
 
 export const tasksReducer = tasksSlice.reducer;
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
-module.exports = {
-    "name": "webpack",
-    "version": "5.89.0",
-    "author": "Tobias Koppers @sokra",
+// module.exports = {
+//     "name": "webpack",
+//     "version": "5.89.0",
+//     "author": "Tobias Koppers @sokra",
     
-    "resolve": {
-        "fallback": {
-            "http": require.resolve('stream-http')
-        }
-    },
-};
+//     "resolve": {
+//         "fallback": {
+//             "http": require.resolve('stream-http')
+//         }
+//     },
+// };
